3ccd17921903d94877270c028e05196594455784
surrealdetective <thefifth@gmail.com>
2017-03-07 20:27:53 -0600

    add a basic readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Code to Copy
+
+page.find(:xpath, "//i[contains(@data-target, \"#week_#{x}\")]").click
+
+t.find_css(".task-date").first.all_text
--- scrape.rb
+++ scrape.rb
@@ -18,7 +18,17 @@ def take_screenshot(page, title='screenshot.png')
   page.save_screenshot(title)
 end
 
-# get into app
-visit ""
+# 1. Go to home
+visit "http://my.ilga.gov/"
 sleep 7 # required
 require 'pry'; binding.pry
+
+# 2. Go to House
+
+# 3. Go to Committee Hearings
+
+# 4. Build List of committees to scrape
+
+# 5. For each committee, find potential witness link
+
+# 6. Find witness link title, link, and if it exists
