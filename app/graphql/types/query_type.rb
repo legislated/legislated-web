72e494dcfdf5409a08b6918b9f7e25641a2b1557
Ty Cobb <ty.cobb.m@gmail.com>
2017-03-24 19:44:43 -0500

    move individual node fetches into viewer

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,30 +12,5 @@ module Types
       type ViewerType
       resolve -> (obj, args, ctx) { Object.new }
     end
-
-    # individual nodes
-    field :chamber do
-      type ChamberType
-      argument :id, !types.ID, "The graph id of the chamber"
-      resolve -> (obj, args, ctx) { Chamber.find(args["id"]) }
-    end
-
-    field :committee do
-      type CommitteeType
-      argument :id, !types.ID, "The graph id of the committee"
-      resolve -> (obj, args, ctx) { Committee.find(args["id"]) }
-    end
-
-    field :hearing do
-      type HearingType
-      argument :id, !types.ID, "The graph id of the hearing"
-      resolve -> (obj, args, ctx) { Hearing.find(args["id"]) }
-    end
-
-    field :bill do
-      type BillType
-      argument :id, !types.ID, "The graph id of the bill"
-      resolve -> (obj, args, ctx) { Bill.find(args["id"]) }
-    end
   end
 end
--- app/graphql/types/viewer_type.rb
+++ app/graphql/types/viewer_type.rb
@@ -5,6 +5,32 @@ module Types
     global_id_field :id
     interfaces [GraphQL::Relay::Node.interface]
 
+    # entities
+    field :chamber do
+      type ChamberType
+      argument :id, !types.ID, "The graph id of the chamber"
+      resolve -> (obj, args, ctx) { Chamber.find(args["id"]) }
+    end
+
+    field :committee do
+      type CommitteeType
+      argument :id, !types.ID, "The graph id of the committee"
+      resolve -> (obj, args, ctx) { Committee.find(args["id"]) }
+    end
+
+    field :hearing do
+      type HearingType
+      argument :id, !types.ID, "The graph id of the hearing"
+      resolve -> (obj, args, ctx) { Hearing.find(args["id"]) }
+    end
+
+    field :bill do
+      type BillType
+      argument :id, !types.ID, "The graph id of the bill"
+      resolve -> (obj, args, ctx) { Bill.find(args["id"]) }
+    end
+
+    # connections
     connection :chambers, -> { ChamberType.connection_type } do
       description "All chambers"
       resolve -> (obj, ctx, args) { Chamber.all }
