360fea6c11f7a3827e9d4b28ac06aebbd3092052
Ty Cobb <ty.cobb.m@gmail.com>
2017-04-22 09:43:36 -0500

    add support for searching resolutions

--- app/services/bills_search_service.rb
+++ app/services/bills_search_service.rb
@@ -1,13 +1,16 @@
 class BillsSearchService
   def self.filter(query, search_query)
-    search_query = search_query.downcase
-
-    if search_query.start_with?('sb', 'hb')
-      query.where('LOWER(document_number) LIKE :q', q: "%#{search_query}%")
+    search_query = search_query.strip.downcase
+    if document?(search_query)
+      query.where('document_number ILIKE :q', q: "%#{search_query}%")
     else
       q = query.by_keyword(search_query)
       q = query.by_fuzzy_title(search_query) if q.empty?
       q
     end
+  end
+
+  def self.document?(search_query)
+    search_query.match(/^[hs][br]\d*$/).present?
   end
 end
--- spec/services/bills_search_service_spec.rb
+++ spec/services/bills_search_service_spec.rb
@@ -2,38 +2,35 @@ describe BillsSearchService do
   subject { described_class }
 
   describe '#filter' do
-    let(:query) { Bill.all }
-    let(:search_query) { '' }
-    let(:result) { subject.filter(query, search_query) }
+    def search (search_query)
+      subject.filter(Bill.all, search_query)
+    end
 
     context 'when the search query looks like plain text' do
       context 'and the title prefix matches' do
-        let(:search_query) { 'MOTO' }
         let!(:bill1) { create(:bill, :with_any_hearing, title: 'MoTor AwAY') }
         let!(:bill2) { create(:bill, :with_any_hearing, title: 'I am a ScIEntiSt') }
 
         it 'includes the bill' do
-          expect(result).to eq([bill1])
+          expect(search('MOTO')).to eq([bill1])
         end
       end
 
       context 'and the title fuzzy matches' do
-        let(:search_query) { 'OTOR' }
         let!(:bill1) { create(:bill, :with_any_hearing, title: 'MoTor AwAY') }
         let!(:bill2) { create(:bill, :with_any_hearing, title: 'I am a ScIEntiSt') }
 
         it 'includes the bill' do
-          expect(result).to eq([bill1])
+          expect(search('OTOR')).to eq([bill1])
         end
       end
 
       context "and the bill's summary prefixes" do
-        let(:search_query) { 'STREE' }
         let!(:bill1) { create(:bill, :with_any_hearing, summary: '...dOwn the ICy stREets.') }
         let!(:bill2) { create(:bill, :with_any_hearing, summary: '...I seEK to uNDeRstand me') }
 
         it 'includes the bill' do
-          expect(result).to eq([bill1])
+          expect(search('STREE')).to eq([bill1])
         end
       end
     end
@@ -41,21 +38,23 @@ describe BillsSearchService do
     context 'when the search query looks like a document number' do
       let!(:bill1) { create(:bill, :with_any_hearing, document_number: 'HB1234') }
       let!(:bill2) { create(:bill, :with_any_hearing, document_number: 'SB1234') }
+      let!(:resolution1) { create(:bill, :with_any_hearing, document_number: 'HR1234') }
+      let!(:resolution2) { create(:bill, :with_any_hearing, document_number: 'SR1234') }
 
-      context 'for the house' do
-        let(:search_query) { 'hB' }
-
-        it 'includes house bills' do
-          expect(result).to eq([bill1])
-        end
+      it 'includes house bills' do
+        expect(search('HB')).to eq([bill1])
       end
 
-      context 'for the senate' do
-        let(:search_query) { 'Sb' }
+      it 'includes house resolutions' do
+        expect(search('HR')).to eq([resolution1])
+      end
 
-        it 'includes senate bills' do
-          expect(result).to eq([bill2])
-        end
+      it 'includes senate bills' do
+        expect(search('SB')).to eq([bill2])
+      end
+
+      it 'includes senate bills' do
+        expect(search('SR')).to eq([resolution2])
       end
     end
   end
