b6aec204326aa3b134883aa79591bf17d2ec034a
Ty Cobb <ty.cobb.m@gmail.com>
2017-10-22 15:46:17 -0500

    fix test name

--- spec/integration/import_legislators_spec.rb
+++ spec/integration/import_legislators_spec.rb
@@ -1,7 +1,7 @@
 describe 'importing legislators', :json_snapshot do
   subject { ImportLegislatorsJob.new }
 
-  it 'imports bills from openstates' do
+  it 'imports legislators from openstates' do
     snapshot = load_snapshot('import_legislators.json')
 
     VCR.use_cassette('import_legislators') do
