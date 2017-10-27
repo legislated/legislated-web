c1e149db4641f51b99d51ba5d93cc10e5cfb646a
David Corwin <dshvazz@gmail.com>
2017-10-26 21:06:32 -0500

    ignore moment locales

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -3,6 +3,7 @@ const { environment } = require('@rails/webpacker')
 const merge = require('webpack-merge')
 const path = require('path')
 const RelayCompilerPlugin = require('relay-compiler-webpack-plugin')
+const webpack = require('webpack')
 
 const client = './app/javascript'
 const config = merge(environment.toWebpackConfig(), {
@@ -15,7 +16,8 @@ const config = merge(environment.toWebpackConfig(), {
     new RelayCompilerPlugin({
       src: path.resolve(client, './src'),
       schema: path.resolve('./schema.json')
-    })
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ]
 })
 
