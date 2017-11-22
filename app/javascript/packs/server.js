18b2c4b2bfd663b2c47d5085f5105065025f4f16
Ty Cobb <ty.cobb.m@gmail.com>
2017-11-21 21:17:31 -0600

    use forked hypernova

--- app/javascript/packs/server.js
+++ app/javascript/packs/server.js
@@ -3,6 +3,7 @@ import '../server-polyfills'
 import React from 'react'
 import { renderReact } from 'hypernova-react'
 import { StaticRouter } from 'react-router-dom'
+import { renderStatic } from 'glamor/server'
 import { App } from '../src/App'
 
 type Props = {
@@ -16,4 +17,16 @@ export default renderReact('server', ({ location }: Props) => (
   >
     <App />
   </StaticRouter>
-))
+), {
+  serialize (renderToString, serializeHtml) {
+    const { html, css, ids } = renderStatic(renderToString)
+
+    return `
+      <head><style>${css}</style></head>
+      <body>
+        ${serializeHtml(html)}
+        <script>window._glam = ${JSON.stringify(ids)}</script>
+      </body>
+    `
+  }
+})
