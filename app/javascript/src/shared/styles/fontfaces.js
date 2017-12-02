5988aa33b133edd0785520e530bf3855972a239f
Luke Ramus <luke.ramus@gmail.com>
2017-12-02 09:23:39 -0600

    shared index, camel-case

--- app/javascript/src/shared/styles/fontfaces.js
+++ app/javascript/src/shared/styles/fontfaces.js
@@ -1,20 +1,6 @@
 // @flow
 import { css } from 'glamor'
-import robotoLightWOFF2 from '../../../fonts/roboto-light.woff2'
-import robotoLightWOFF from '../../../fonts/roboto-light.woff'
-import robotoLightTTF from '../../../fonts/roboto-light.ttf'
-
-import robotoRegularWOFF2 from '../../../fonts/roboto-regular.woff2'
-import robotoRegularWOFF from '../../../fonts/roboto-regular.woff'
-import robotoRegularTTF from '../../../fonts/roboto-regular.ttf'
-
-import robotoBoldWOFF2 from '../../../fonts/roboto-bold.woff2'
-import robotoBoldWOFF from '../../../fonts/roboto-bold.woff'
-import robotoBoldTTF from '../../../fonts/roboto-bold.ttf'
-
-import zillaSlabRegularWOFF2 from '../../../fonts/zillaslab-regular.woff2'
-import zillaSlabRegularWOFF from '../../../fonts/zillaslab-regular.woff'
-import zillaSlabRegularTTF from '../../../fonts/zillaslab-regular.ttf'
+import * as fonts from '../../../fonts'
 
 css.insert(`
 @font-face {
@@ -22,35 +8,31 @@ css.insert(`
   font-style: normal;
   font-weight: 300;
   src:local('Roboto Light'), local('Roboto-Light'),
-      url('${robotoLightWOFF2}') format('woff2'),
-      url('${robotoLightWOFF}') format('woff'),
-      url('${robotoLightTTF}') format('truetype');
+      url('${fonts.robotoLightWoff2}') format('woff2'),
+      url('${fonts.robotoLightWoff}') format('woff');
 }
 @font-face {
   font-family: 'Roboto';
   font-style: normal;
   font-weight: 400;
   src:local('Roboto'), local('Roboto-regular'),
-      url('${robotoRegularWOFF2}') format('woff2'),
-      url('${robotoRegularWOFF}') format('woff'),
-      url('${robotoRegularTTF}') format('truetype');
+      url('${fonts.robotoRegularWoff2}') format('woff2'),
+      url('${fonts.robotoRegularWoff}') format('woff');
 }
 @font-face {
   font-family: 'Roboto';
   font-style: normal;
   font-weight: 700;
   src:local('Roboto Bold'), local('Roboto-Bold'),
-      url('${robotoBoldWOFF2}') format('woff2'),
-      url('${robotoBoldWOFF}') format('woff'),
-      url('${robotoBoldTTF}') format('truetype');
+      url('${fonts.robotoBoldWoff2}') format('woff2'),
+      url('${fonts.robotoBoldWoff}') format('woff');
 }
 @font-face {
   font-family: 'Zilla Slab';
   font-style: normal;
   font-weight: 400;
   src:local('Zilla Slab'), local('ZillaSlab-Regular'),
-      url('${zillaSlabRegularWOFF2}') format('woff2'),
-      url('${zillaSlabRegularWOFF}') format('woff'),
-      url('${zillaSlabRegularTTF}') format('truetype');
+      url('${fonts.zillaSlabRegularWoff2}') format('woff2'),
+      url('${fonts.zillaSlabRegularWoff}') format('woff');
 }
 `)
