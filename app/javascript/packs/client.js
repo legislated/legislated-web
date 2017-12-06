3c2eb624888e46b3cd37b82b9e282f83eb47527b
Ty Cobb <ty.cobb.m@gmail.com>
2017-12-05 18:27:09 -0600

    fix case-sensitivity problem

--- app/javascript/packs/client.js
+++ app/javascript/packs/client.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { renderReact } from 'hypernova-react'
 import { BrowserRouter } from 'react-router-dom'
 import { ScrollContext } from 'react-router-scroll-4'
-import { App } from '../src/app'
+import { App } from '../src/App'
 
 export default renderReact('client', () => (
   <BrowserRouter>
