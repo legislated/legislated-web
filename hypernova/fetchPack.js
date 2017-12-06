3bc83c80f4e25de0e0e3f9ce4939b6abe6e74608
Ty Cobb <ty.cobb.m@gmail.com>
2017-12-05 22:56:31 -0600

    add fetchPack function that loads file from filesystem when dev server is not available

---
+++ hypernova/fetchPack.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const fs = require('fs')
+const http = require('http')
+const concat = require('concat-stream')
+
+// pull paths out of the webpack config
+const env = process.env.NODE_ENV || 'development'
+const config = require(path.resolve(`./config/webpack`, env))[0]
+const { devServer, output } = config
+const { path: packsPath } = output
+
+// pack helpers
+function requireManifest () {
+  return require(path.resolve(packsPath, 'manifest.json'))
+}
+
+function packNameFromManifest (manifest, name) {
+  return manifest[`${name}.js`]
+}
+
+// require branches
+function requireFromDevServer ({ host, port }) {
+  const devServerUrl = `http://${host}:${port}`
+
+  return async (name) => {
+    const packFile = packNameFromManifest(requireManifest(), name)
+    const filePath = `${devServerUrl}${packFile}`
+
+    return new Promise((resolve, reject) => {
+      http.get(url, (res) => {
+        res.setEncoding('utf8')
+        res.pipe(concat({ encoding: 'string' }, (data) => {
+          resolve(data)
+        }))
+      })
+    })
+  }
+}
+
+function requireFromFileSystem () {
+  const manifest = requireManifest()
+
+  return async (name) => {
+    // both packsPath and packFIle have a leading slash
+    const packFile = packNameFromManifest(manifest, name)
+    const filePath = path.join(packsPath, '..', packFile)
+
+    return new Promise((resolve, reject) => {
+      fs.readFile(filePath, 'utf8', (error, data) => {
+        if (data) {
+          resolve(data)
+        } else {
+          reject(error)
+        }
+      })
+    })
+  }
+}
+
+module.exports = devServer ? requireFromDevServer(devServer) : requireFromFileSystem()
--- hypernova/hypernova.js
+++ hypernova/hypernova.js
@@ -1,13 +1,6 @@
 const { resolve } = require('path')
 const hypernova = require('hypernova/server')
-const requireFromUrl = require('./requireFromUrl')
-
-// load webpack config
-const env = process.env.NODE_ENV || 'development'
-const configs = require(resolve(`./config/webpack`, env))
-const { devServer, output } = configs[0]
-const { path: packsPath } = output
-const devServerUrl = `http://${devServer.host}:${devServer.port}`
+const fetchPack = require('./fetchPack')
 
 // run
 const { run } = hypernova.createVM({ cacheSize: 1 })
@@ -17,25 +10,8 @@ hypernova({
   devMode: true,
   port: 3030,
   getComponent: async (name) => {
-    const manifest = require(resolve(packsPath, 'manifest.json'))
-    const packName = manifest[`${name}.js`]
-
-    if (packName == null) {
-      console.log(`ssr [name: ${name}] missing pack in manifest`, manifest)
-    }
-
-    const filePath = `${devServerUrl}${packName}`
-    console.log(`ssr [name: ${name}] fetching ${filePath}`)
-    const pack = await requireFromUrl(filePath)
-    console.log(`ssr [name: ${name}] loading pack`)
+    const pack = await fetchPack(name)
     const { default: Component } = run(name, pack)
-
-    if (Component !== null) {
-      console.log(`ssr [name: ${name}] loaded!`)
-    } else {
-      console.log(`ssr [name: ${name}] failed to load`)
-    }
-
     return Component
   }
 })
