584ab699ce08d08129b9a57d8d66ce86e2ad1778
Ty Cobb <ty.cobb.m@gmail.com>
2017-12-11 23:20:30 -0600

    disable dev plugin in non-dev

--- config/initializers/hypernova.rb
+++ config/initializers/hypernova.rb
@@ -1,6 +1,8 @@
-require 'hypernova/plugins/development_mode_plugin'
+if Rails.env.development?
+  require 'hypernova/plugins/development_mode_plugin'
+  Hypernova.add_plugin!(DevelopmentModePlugin.new)
+end
 
-Hypernova.add_plugin!(DevelopmentModePlugin.new)
 Hypernova.configure do |config|
   config.host = "localhost"
   config.port = 3030
