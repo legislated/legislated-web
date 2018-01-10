df86bf3a45d99091fc724a08241f78e3c0088881
akwandmd <akwan@dmd.com>
2018-01-09 20:31:00 -0600

    added skeletons for updating relations and job scheduling

--- app/jobs/import_committees_job.rb
+++ app/jobs/import_committees_job.rb
@@ -5,11 +5,30 @@ class ImportCommitteesJob
     @open_states_service = open_states_service
   end
 
-  def merge(id)
+  def self.scheduled?
+    Time.current.saturday?
+  end
 
+  def perform
+    committee_attrs = @open_states_service
+      .fetch_committees(fields: fields)
+      .map { |data| parse_attributes(data) }
+      .reject(&:nil?)
+
+    committee_attrs.each do |attrs|
+      Committee.upsert_by!(:os_id, attrs)
+    end
+  end
+
+  def merge(id)
+    # update committee table with data from scraped committee websites
   end
 
   private
+
+  def update_membership
+    # update the member relation between committee and legislator
+  end
 
   def parse_attributes(data)
     attrs = {
