ef63d22c083dc6043588fa02214d0ce6cee55720
Ty Cobb <ty.cobb.m@gmail.com>
2018-02-08 08:55:43 -0600

    upgrade watchman version on ci

--- scripts/ci-watchman.sh
+++ scripts/ci-watchman.sh
@@ -4,6 +4,6 @@ set -e
 WATCHMAN_PATH=~/facebook/watchman
 git clone https://github.com/facebook/watchman.git $WATCHMAN_PATH
 pushd $WATCHMAN_PATH
-git checkout v4.7.0
+git checkout v4.9.0
 ./autogen.sh && ./configure && make && sudo make install
 popd
