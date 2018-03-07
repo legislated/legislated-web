33442fdc3fe03478c52c40864819ef31d5438b0b
Ty Cobb <ty.cobb.m@gmail.com>
2018-03-07 01:36:18 -0600

    squash migrations

--- db/migrate/20170314001653_create_chamber.rb
+++
@@ -1,9 +0,0 @@
-class CreateChamber < ActiveRecord::Migration[5.0]
-  def change
-    create_table :chambers, id: :uuid do |t|
-      t.column :name, :string, null: false
-      t.column :key, :string, null: false
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20170502084107_add_witness_slip_result_url_to_bill.rb
+++
@@ -1,5 +0,0 @@
-class AddWitnessSlipResultUrlToBill < ActiveRecord::Migration[5.0]
-  def change
-    add_column :bills, :witness_slip_result_url, :string
-  end
-end
