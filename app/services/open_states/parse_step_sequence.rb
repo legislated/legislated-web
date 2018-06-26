bcea3c707e11c7e2bce28cb02cf80494aea43f90
Ty Cobb <ty.cobb.m@gmail.com>
2018-06-26 17:57:32 -0500

    wip seqeuencing algorithm

--- app/services/open_states/parse_step_sequence.rb
+++ app/services/open_states/parse_step_sequence.rb
@@ -1,7 +1,55 @@
 module OpenStates
   class ParseStepSequence
+    # upper introduced
+    # upper:committee introduced
+    # upper:committee resolved
+    # upper resolved
+    # lower introduced
+    # lower:committee introducexed
+    # lower:committee resolved
+    # lower resolved
+    # upper introduced
+
+    # upper introduced
+    # upper:committee introduced
+    # upper:committee resolved
+    # upper resolved
+    # lower introduced
+    # lower:committee introduced
+    # lower:committee resolved
+    # lower resolved
+    # upper introduced
+    # upper:committee introduced
+    # upper:committee resolved
+    # upper resolved
+    # governor introduced
+    # governor resolved vetoed
+
+    # upper resolved
+    # upper:committee resolved
+    # goal know which chambers are passed and which are open
     def call(steps)
-      steps
+      sequence = []
+
+      # track the actors with an introduced action
+      introduced_actors = Set.new
+
+      # assume sorted by ascending date
+      steps.reverse_each do |step|
+        # if we encounter an actor we've already introduced, we hit a cycle
+        if introduced_actors.include(step.actor)
+          break
+        end
+
+        if step.action == Step::Actions::INTRODUCED
+          introduced_actors.add(step.actor)
+        end
+
+        # add this step to the front of the sequence to preserve ascending order
+        sequence.unshift(step)
+      end
+
+      sequence
     end
   end
 end
--- spec/services/open_states/parse_step_sequence_spec.rb
+++ spec/services/open_states/parse_step_sequence_spec.rb
@@ -2,18 +2,67 @@ describe OpenStates::ParseStepSequence do
   subject { described_class.new }
 
   describe '#call' do
-    xit 'parses steps into a valid sequence' do
+    it 'parses steps into a valid sequence' do
       steps = [
         build(:open_states_step, {
           actor: Step::Actors::LOWER,
           action: Step::Actions::INTRODUCED,
           resolution: Step::Resolutions::NONE,
           date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::LOWER_COMMITTEE,
+          action: Step::Actions::INTRODUCED,
+          resolution: Step::Resolutions::NONE,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::LOWER_COMMITTEE,
+          action: Step::Actions::RESOLVED,
+          resolution: Step::Resolutions::PASSED,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::LOWER,
+          action: Step::Actions::RESOLVED,
+          resolution: Step::Resolutions::PASSED,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::UPPER,
+          action: Step::Actions::INTRODUCED,
+          resolution: Step::Resolutions::NONE,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::UPPER_COMMITTEE,
+          action: Step::Actions::INTRODUCED,
+          resolution: Step::Resolutions::NONE,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::UPPER_COMMITTEE,
+          action: Step::Actions::RESOLVED,
+          resolution: Step::Resolutions::PASSED,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::UPPER,
+          action: Step::Actions::RESOLVED,
+          resolution: Step::Resolutions::PASSED,
+          date: Time.now
+        }),
+        build(:open_states_step, {
+          actor: Step::Actors::LOWER,
+          action: Step::Actions::INTRODUCED,
+          resolution: Step::Resolutions::PASSED,
+          date: Time.now
         })
       ]
 
       actual = subject.call(steps)
-      expect(actual.map(&:to_h)).to eq([])
+      binding.pry
+      expect(actual.map(&:to_h)).to eq(steps.drop(4).map(&:to_h))
     end
   end
 end
