10ed88909c73489d8b44afe0347635c657608f0d
akwandmd <akwan@dmd.com>
2018-07-25 12:21:05 -0500

    refactored factory bot for committees

--- spec/factories/committees.rb
+++ spec/factories/committees.rb
@@ -1,24 +1,9 @@
 FactoryBot.define do
   factory :committee do
     ilga_id { Faker::Number.unique.number(5) }
-    os_id { Faker::Number.unique.number(9) }
-    # os_id { Faker::String.random(9) }
+    os_id { Faker::Code.asin }
     name { Faker::Company.industry }
     # chamber { Chamber.all.sample }
-
-    # factory :fetched_ilga_committee do
-    #   transient do
-    #     chamber nil
-    #   end
-
-    #   initialize_with do
-    #     Ilga::ParseHearing::Committee.new(
-    #       ilga_id,
-    #       os_id,
-    #       name
-    #     )
-    #   end
-    # end
 
     factory :open_states_committee do
       transient do
